e06f802e0c9576732ef7fa7c9af13f4bc10a6b77
Felix Wambiri <FelixWambiri>
2018-05-22 22:40:27 +0300

    [Feature] Add code to implement deleting event

--- src/actions/events.js
+++ src/actions/events.js
@@ -10,6 +10,18 @@ const eventsFetched = (data) => ({
     data
 });
 
+const requestFailed = error =>({type:"REQUEST_FAILED"})
+
+export const eventDeleted = ()=>({type:"EVENT_DELETED"})
+
+
+
 export const addEvent = (data) => (dispatch) => api.user.addEvent(data);
 
 export const fetchEvents = () => (dispatch) => api.events.fetchAll().then(events => dispatch(eventsFetched(events)));
+
+export const deleteEvent = (id)=> {
+    return dispatch =>{
+        return api.events.delete(id).then(()=>dispatch(eventDeleted())).catch(error=>dispatch(error.message));
+    }
+}
