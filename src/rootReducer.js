ce129f771ea5337cb9668c10a60b3ef47d6f1c8a
Felix Wambiri <FelixWambiri>
2018-06-06 23:55:19 +0300

    [Feature] Add event reducers

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,19 @@
 import {combineReducers} from 'redux';
 import user from "./reducers/user";
 import events from "./reducers/events";
-
+import loading from "./reducers/loading.reducer"
+import event from "./reducers/event.reducer"
+import guests from "./reducers/guest";
+import myEvents from "./reducers/my_events.reducer"
+import searching from "./reducers/searchLoading.reducer";
+import error from "./reducers/error.reducer"
 export default combineReducers({
     user,
-    events
+    events,
+    event,
+    guests,
+    myEvents,
+    error,
+    searching,
+    loading
 });
