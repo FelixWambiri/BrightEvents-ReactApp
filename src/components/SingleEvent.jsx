8286a292a90deae873d18561ab1df1f8ef3b5fa9
Felix Wambiri <FelixWambiri>
2018-06-06 23:53:56 +0300

    [Feature] Add a presentational component

--- src/components/SingleEvent.jsx
+++ src/components/SingleEvent.jsx
@@ -1,30 +1,61 @@
-import React from "react"
-export default (props)=>{
-    const {event} = props;
-    return (
-        <div className="card">
-                            <div className="content" key={event.id}>
-                                <a className="header">{event.name}</a>
-                            <div className="meta">
-                                <span className="body">{event.category}</span>
-                            </div>
-                            <div className="meta">
-                                <span className="body">{event.location}</span>
-                            </div>
-                            <div className="meta">
-                                <span className="date">{event.date_hosted}</span>
-                            </div>
-                            <div className="description">
-                                {event.description}
-                            </div>
-                            </div>
-                            <div className="extra content">
-                                <a>
-                                    <i className="user icon"></i>
-                                    22 Friends
-                                </a>
-                            </div>
-                            <button type="button">Delete</button>
-                        </div>
-    )
-}
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Icon } from 'semantic-ui-react';
+import ModalExampleShorthand from '../components/pages/RsvpsModal';
+import eventHolder from '../assets/events.png';
+
+const hdate = require('human-date');
+
+export default (props) => {
+  const { event, onDelete, showReservations } = props;
+  function handleDelete(id) {
+    onDelete(id);
+  }
+  function handleShowReservations(id) {
+    showReservations(id);
+  }
+  return (
+    <Card>
+      <img src={eventHolder} />
+      <Card.Content>
+        <Card.Header>
+          {event.name}
+        </Card.Header>
+        <Card.Meta>
+          <span className="date">
+            {event.category}
+          </span>
+        </Card.Meta>
+        <Card.Meta>
+          <span className="date">
+            {event.location}
+          </span>
+        </Card.Meta>
+        <Card.Description>
+          {event.description}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="calendar" />
+          {hdate.relativeTime(event.date_hosted)}
+        </a>
+      </Card.Content>
+      <Card.Content extra>
+        <div className="extra content">
+          <a>
+            <Link to={`/event/${event.id}/rsvp`}><i className="favorite icon" />Attendees</Link>
+          </a>
+          <hr />
+          <div className="ui fluid buttons">
+            <Link to={`/events/edit/${event.id}`} className="ui primary icon button"> <i className="edit icon" /> Edit</Link>
+            <div className="or" />
+            <button onClick={() => handleDelete(event.id)} className="ui red button"><i className="trash icon" /> Delete</button>
+          </div>
+        </div>
+      </Card.Content>
+    </Card>
+
+  );
+};
+
