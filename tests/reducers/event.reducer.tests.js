c34a28f507bfccf7ae3d94395aaa59a8d8bb8a4c
Felix Wambiri <FelixWambiri>
2018-06-06 23:33:55 +0300

    [Chore] Add tests for the functionality

---
+++ tests/reducers/event.reducer.tests.js
@@ -0,0 +1,13 @@
+import expect from 'expect';
+import * as actions from "../../src/actions/events";
+import  eventReducer from "../../src/reducers/event.reducer";
+
+describe('eventReducer', ()=> {
+    it('should return one specific event', ()=> {
+        const initialState = {};
+        const event = {name : 'another name'};
+        const action = actions.singleEventFetched(event);
+        const newState = eventReducer(initialState, action);
+        expect(newState).toEqual(event);
+    });
+});
