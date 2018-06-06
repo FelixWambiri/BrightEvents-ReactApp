7cd0d5fcfc15083e33e92bef03eb78dbf82e4ae4
Felix Wambiri <FelixWambiri>
2018-06-06 23:49:02 +0300

    [Feature] Enable redirects after successful requests

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import {allEventsSelector} from '../../reducers/events';
 import AddEventsCtA from '../ctas/AddEventsCtA';
-import {Segment} from 'semantic-ui-react';
-import SearchEventForm from '../forms/SearchEventForm';
+import {Segment, Button, Message} from 'semantic-ui-react';
 import EventForm from '../forms/EventForm';
-import {fetchEvents} from '../../actions/events';
+import {fetchEvents, searchEvents} from '../../actions/events';
 import SingleEvent from '../SingleEvent';
-
+import Loader from "../../components/Loader";
+import {deleteEvent} from "../../actions/events";
+import {showReservations} from "../../actions/events";
 class DashboardPage extends React.Component{
     constructor(props){
     super(props)
@@ -17,24 +18,31 @@ class DashboardPage extends React.Component{
     }
   }
     componentDidMount = () => {
-        // this.onInit(this.props);
-
         this.props.fetchEvents();
     }
-    // onEventSelect = event => this.setState({event});
     render(){
-        const {events} = this.props
-        console.log('>>>', this.props.events)
+        const {events,isLoading,deleteEvent,showReservations} = this.props
+        if (isLoading){
+            return <Loader/>
+        }
+
         return(
         <div>
             <AddEventsCtA/>
-            <SearchEventForm onEventSelect = {this.onEventSelect}/>
+            {/* <SearchEventForm onSearch={searchEvents} onEventSelect = {this.onEventSelect}/> */}
                 {this.state.event && (<EventForm submit = {this.addEvent} event = {this.state.event}/>)}
-
+                
             <div className="ui cards">
             {
-                events.length > 0 && 
-                events.map(event => <SingleEvent event={event}/>)
+                events.length > 0 ? 
+                events && 
+                events.map(event => <SingleEvent key={event.id} onDelete={deleteEvent} event={event} showReservations={showReservations}/>)
+                :
+                <Message warning className="ui fluid container">
+                    <Message.Header>You have not created any Event Yet!</Message.Header>
+                    <p>To add events click on the plus button above.</p>
+                </Message>
+               
             }
             </div>
         </div>
@@ -42,17 +50,20 @@ class DashboardPage extends React.Component{
 }
         
 DashboardPage.propTypes ={
-    fetchEvents : PropTypes.func.isRequired,
-    events: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired
-    }).isRequired).isRequired
+    fetchEvents : PropTypes.func.isRequired
 };
 
-// we are going to use selectors
+const mapDispatchToProps = dispatch=>({
+    fetchEvents:()=>dispatch(fetchEvents()),
+    deleteEvent:(id)=>dispatch(deleteEvent(id)),
+    showReservations:(id)=>dispatch(showReservations(id))
+})
+
 function mapStateToProps(state){
     return{
-        events: allEventsSelector(state)
+        events: state.myEvents,
+        isLoading:state.loading,
     };
 };
 
-export default connect(mapStateToProps, {fetchEvents})(DashboardPage);
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
